b71992f2a409a286562af1c470a6afb31b0ea61a
Christian <chrbudtz@gmail.com>
2015-09-29 11:03:11 +0200

    building model

---
+++ WpfApplication1/ClassLibrary1/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class
+    {
+        private String name;
+        private String stereotype;
+        private Boolean isAbstract;
+
+        public string Name  { get { return name; } set { name = value; } }
+
+        public string StereoType { get { return stereotype; } set { stereotype = value; } }
+
+        public bool IsAbstract { get { return isAbstract; } set { isAbstract = value;
+            }
+        }
+    }
+}
