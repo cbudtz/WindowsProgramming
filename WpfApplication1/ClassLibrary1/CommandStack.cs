884d12b2b98d956399800e238726c2c9bd07d50e
Christian <chrbudtz@gmail.com>
2015-10-06 08:52:12 +0200

    Now with exciting new CommandStack

---
+++ WpfApplication1/ClassLibrary1/CommandStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModelLibrary
+{
+    class CommandStack
+    {
+        ICommand root;
+        ICommand active;
+        LinkedList<ICommand> undone;
+        
+        public void setActive(ICommand command)
+        {
+            //TODO: Implement recursice function to crawl up and down tree;
+        }
+
+        public void save()
+        {
+            //TODO: implement save of whole stack
+
+        }
+        public void undo()
+        {
+            //TODO: Implemnt
+        }
+
+        public void redo()
+        {
+            //TODO:Implement - uses undone list
+        }
+
+    }
+}
