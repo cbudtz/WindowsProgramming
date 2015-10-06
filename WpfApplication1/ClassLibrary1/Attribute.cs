ec6019b644b7cbc321bdadfc4b5ca96139c00c2d
Christian <chrbudtz@gmail.com>
2015-10-06 09:26:38 +0200

    Attributes

---
+++ WpfApplication1/ClassLibrary1/Attribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    class Attribute
+    {
+        String type;
+        String name;
+        Visibility visibility;
+        List<Modifier> modifiers;
+    }
+}
---
+++ WpfApplication1/ClassLibrary1/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    class Connection
+    {
+        Class start;
+        String startMultiplicity;
+        String endMultiplicity;
+        Class end;
+        ConnectionType type;
+    }
+}
