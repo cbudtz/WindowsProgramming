7d3a78ee50b37233f42dcdd3c543720ce5484376
mhesselbjerg <m_hesselbjerg@hotmail.com>
2015-10-12 13:50:34 +0200

    uml

---
+++ SoftwareModeler/SoftwareModeler/Commands/AddUMLClass.cs
@@ -0,0 +1,26 @@
+﻿using Area51.SoftwareModeler.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area51.SoftwareModeler.Commands
+{
+    class AddUMLClassCommand
+    {
+        private ObservableCollection<Class> classes;
+
+        private Class repClass;
+
+        public AddUMLClassCommand(ObservableCollection<Class> _classes, Class _class)
+        {
+            classes = _classes;
+            repClass = _class;
+        }
+
+
+
+    }
+}
