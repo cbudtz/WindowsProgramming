ea5d282dfbd4c779b0c288fbdad9f57a7e4ba251
Rúni Egholm Vørmadal <runivormadal@outlook.com>
2015-10-12 12:55:16 +0200

    added new command

---
+++ SoftwareModeler/SoftwareModeler.Model/Commands/moveShapeCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area51.SoftwareModeler.Models
+{
+    public class MoveShapeCommand : ICommandExt
+    {
+        private Shape shape;
+        private double xOffset;
+        private double yOffset;
+        private ICommandExt parent;
+        private List<ICommandExt> children;
+    
+        private ICommandExt Parent { get { return parent; } }
+        private List<ICommandExt> Children { get { return children; } }
+
+
+        public MoveShapeCommand(ICommandExt _parent, Shape _shape, double _xOffset, double _yOffset)
+        {
+            parent = _parent;
+            children = new List<ICommandExt>();
+            shape = _shape;
+            xOffset = _xOffset;
+            yOffset = _yOffset;
+        }
+        public void addChild(ICommandExt child)
+        {
+            children.Add(child);
+        }
+
+        public bool canExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void execute()
+        {
+            shape.CanvasCenterX += xOffset;
+            shape.CanvasCenterY += yOffset;
+        }
+
+        public void unExecute()
+        {
+            shape.CanvasCenterX -= xOffset;
+            shape.CanvasCenterY -= yOffset;
+        }
+    }
+}
+
