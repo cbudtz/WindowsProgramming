ef33b0c909773ccd0ba38b24d82a4573e115ea4c
Christian <chrbudtz@gmail.com>
2015-10-12 12:55:39 +0200

    Implements serializable

--- SoftwareModeler/SoftwareModeler.Model/DummyCommand.cs
+++ SoftwareModeler/SoftwareModeler.Model/DummyCommand.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace Area51.SoftwareModeler.Models
 {
    public class DummyCommand : ICommandExt
     {
         public event EventHandler CanExecuteChanged;
+
 
         public void addChild(ICommandExt child)
         {
@@ -36,7 +39,56 @@ namespace Area51.SoftwareModeler.Models
             throw new NotImplementedException();
         }
 
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteStartElement(this.GetType().Name);
+            //TODO: Fill in attributes
+            writer.WriteEndElement();
+        }
+
         public void unExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        void ICommandExt.addChild(ICommandExt child)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICommandExt.canExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICommandExt.execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ICommandExt> ICommandExt.getChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        ICommandExt ICommandExt.getParent()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICommandExt.unExecute()
         {
             throw new NotImplementedException();
         }
