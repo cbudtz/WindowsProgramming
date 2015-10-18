5253a11af83d2990865919fe674d2bcadf67883b
Rúni Egholm Vørmadal <runivormadal@outlook.com>
2015-10-18 17:12:29 +0200

    pointconverter for multibinding

---
+++ SoftwareModeler/SoftwareModeler.Model/PointConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Area51.SoftwareModeler.Models
+{
+    class PointConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double xValue = (double)values[0];
+            double yValue = (double)values[1];
+            return new Point(xValue, yValue);
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
