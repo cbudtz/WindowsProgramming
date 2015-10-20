fcf66588603815d8e734ad53f02cb38f47bdbe32
Christian <chrbudtz@gmail.com>
2015-10-20 10:04:02 +0200

    Comment class update

--- SoftwareModeler/SoftwareModeler.Model/Comment.cs
+++ SoftwareModeler/SoftwareModeler.Model/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Area51.SoftwareModeler.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Area51.SoftwareModeler.Model
 {
-    class Comment
+    class Comment : Shape
     {
         private string commentString { get; set; }
 
