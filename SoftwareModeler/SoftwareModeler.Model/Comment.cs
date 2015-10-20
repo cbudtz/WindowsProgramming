c77bafe8611ab6940de77b85661331939ebe36e1
Christian <chrbudtz@gmail.com>
2015-10-20 08:46:50 +0200

    Comment Clss

---
+++ SoftwareModeler/SoftwareModeler.Model/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area51.SoftwareModeler.Model
+{
+    class Comment
+    {
+        private string commentString { get; set; }
+
+        public Comment(string commentString)
+        {
+            this.commentString = commentString;
+        }
+    }
+}
