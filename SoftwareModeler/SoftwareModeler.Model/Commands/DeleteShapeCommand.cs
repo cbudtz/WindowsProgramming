dc2c586a4d7074f9f602955812267af6eb8b5346
Christian <chrbudtz@gmail.com>
2015-11-24 10:51:09 +0100

    New deleteShapeCommand

--- SoftwareModeler/SoftwareModeler.Model/Commands/DeleteShapeCommand.cs
+++ SoftwareModeler/SoftwareModeler.Model/Commands/DeleteShapeCommand.cs
@@ -6,46 +6,67 @@ using System.Threading.Tasks;
 using Area51.SoftwareModeler.Models;
 using Area51.SoftwareModeler.Models.Commands;
 using System.Collections.ObjectModel;
+using System.Windows;
 using System.Xml.Serialization;
 
 namespace Area51.SoftwareModeler.Models.Commands
 {
     class DeleteShapeCommand : BaseCommand
     {
-        private ObservableCollection<Shape> shapes;
-        private ObservableCollection<Connection> connections;
-        [XmlIgnore]
-        private Shape shapeToDelete;
-
         public int? shapeID;
         private List<Connection> connectionsToDelete = new List<Connection>();
+        public string ShapeName { get; set; }
+        public double shapeX { get; set; }
+        public double shapeY { get; set; }
+        public double shapewidth { get; set; }
+        public double shapeheight { get; set; }
+        public bool shapeSelected { get; set; }
+        public string shapeStereotype { get; set; }
+        public bool shapeAbstract { get; set; }
+        public Visibility shapeVisibility { get; set; }
 
         public DeleteShapeCommand()
         {
             //Deserialization Constructor.
         }
         
-        public DeleteShapeCommand(Shape _shapeToDelete)
+        public DeleteShapeCommand(Shape shapeToDelete)
         {
-            shapeToDelete = _shapeToDelete;
             shapeID = shapeToDelete.id;
-            shapeToDelete = _shapeToDelete;
+            ShapeName = shapeToDelete.name;
+            shapeX = shapeToDelete.X;
+            shapeY = shapeToDelete.Y;
+            shapewidth = shapeToDelete.Width;
+            shapeheight = shapeToDelete.Height;
+            shapeSelected = shapeToDelete.IsSelected;
+            shapeStereotype = (shapeToDelete as Class).StereoType;
+            shapeAbstract = (shapeToDelete as Class).IsAbstract;
+            shapeVisibility = (shapeToDelete as Class).Visibility;
             // if x,y of either of connection endpoint shapes, remove connection // TODO alternatively use id
-            
+
         }
+
+
 
         public override void execute()
         {
             //Check if deserialized and aquire shape
-            if (shapeToDelete == null) shapeToDelete = ShapeCollector.getI().getShapeByID(shapeID);
+            Shape shapeToDelete = ShapeCollector.getI().getShapeByID(shapeID);
             connectionsToDelete = ShapeCollector.getI().obsConnections.Where(x => (shapeToDelete.X == x.End.X && shapeToDelete.Y == x.End.Y) || (shapeToDelete.X == x.Start.X && shapeToDelete.Y == x.Start.Y)).ToList(); //TODO check if it works
+            foreach (Connection connection in connectionsToDelete)
+            {
+                ShapeCollector.getI().obsConnections.Remove(connection);
+
+            }
             connectionsToDelete.ForEach(x => ShapeCollector.getI().obsConnections.Remove(x));
             ShapeCollector.getI().obsShapes.Remove(shapeToDelete);
         }
 
         public override void unExecute()
         {
-            ShapeCollector.getI().obsShapes.Add(shapeToDelete);
+            Shape shapeToAdd = new Class(shapeID,ShapeName,shapeStereotype,shapeAbstract,new Point(shapeX,shapeY),shapeVisibility );
+            //Create new shape and add it!
+            ShapeCollector.getI().obsShapes.Add(shapeToAdd);
             connectionsToDelete.ForEach(x => ShapeCollector.getI().obsConnections.Add(x));
         }
     }
