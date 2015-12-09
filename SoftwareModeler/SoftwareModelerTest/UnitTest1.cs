3ca300117e24d3a4d2d574bc288e87a74ec4d447
mhesselbjerg <m_hesselbjerg@hotmail.com>
2015-12-09 14:24:04 +0100

    tilføjet testprojekt

---
+++ SoftwareModeler/SoftwareModelerTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Area51.SoftwareModeler.ViewModels;
+using Area51.SoftwareModeler.Views;
+
+namespace SoftwareModelerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartup()
+        {
+
+//            AvalonTestRunner.RunInSTA(
+//delegate
+//{
+    MainWindow window = new MainWindow();
+            window.Visibility = Visibility.Visible;
+    Assert.AreEqual(300, window.Height);
+            //});
+            //MainViewModel mvmModel = new MainViewModel();
+            //mvmModel.NewClassCommand()
+        }
+    }
+}
